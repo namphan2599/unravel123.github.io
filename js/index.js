e440499a5bd5cef2ad0bd9f9723c342e8cc35929
namphan <namphan@123456>
2021-02-18 11:44:05 +0700

    add background effect for welcome section

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
-particlesJS.load('welcome-section', 'assets/particles.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
+const navbar = document.querySelector('.navbar');
+
+window.onscroll = function() {
+  console.log(window.pageYOffset)
+  if (window.pageYOffset > 0) {
+    console.log('> 0');
+    navbar.style.backgroundColor  = '#303841';
+  } else {
+    navbar.style.backgroundColor  = '';
+  }
+}
